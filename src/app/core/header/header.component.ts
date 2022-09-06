5fd3fec5f0d28a1a9eb4451eab8644cff8ff324d
Dovydas Liutkauskas <dovydas.liutkauskas@gmail.com>
2022-09-06 15:12:37 +0300

    feat: dropdown menu implemented

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('dropdown') dropdownEl!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.onclick = (e: MouseEvent) => {
+      if (e.target !== this.dropdownEl.nativeElement) {
+        this.dropdownEl.nativeElement.nextElementSibling.classList.remove(
+          'show'
+        );
+      }
+    };
   }
 
+  shopDropdown(element: HTMLAnchorElement) {
+    if (element.nextElementSibling?.classList.contains('show')) {
+      console.log('stop it');
+    } else {
+      element.nextElementSibling?.classList.add('show');
+    }
+  }
 }
