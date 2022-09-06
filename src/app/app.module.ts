9b20f7582e43ceee51f25474c93b505e64a8e278
Dovydas Liutkauskas <dovydas.liutkauskas@gmail.com>
2022-09-06 10:46:25 +0300

    feat: homepage hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './cores/header/header.component';
-import { FooterComponent } from './cores/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { HeroComponent } from './pages/homepage/hero/hero.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomepageComponent, HeroComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
